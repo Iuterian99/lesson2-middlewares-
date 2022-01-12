b3c033f70e307a7808a0cc291de64dedc3818ff8
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-13 02:14:46 +0500

    see video from 21 minutes

--- express.js
+++ express.js
@@ -4,16 +4,41 @@ const port = 9999;
 
 //! Middlewares
 /*
-routeimiz va controllerimiz"(req, res) => {})"orasida ishlab turuvchi funksiya 
---> authorisation uchun, errorlani ushlab olish uchun, keyinchalik "api" larimizni bloklab qo`yish uchun ishatiladi
+"Middleware"lar routeimiz va controllerimiz"(req, res) => {})"orasida ishlab turuvchi funksiya 
+--> authorisation va authentication uchun, errorlani ushlab olish uchun, keyinchalik "api" larimizni bloklab qo`yish uchun ishatiladi
 */
+
+const users = [
+  {
+    id: 1,
+    name: "Ali",
+  },
+  {
+    id: 2,
+    name: "Bekzod",
+  },
+];
+
 const middleware = (req, res, next) => {
-  console.log("message from middleware");
-  next();
+  const userId = req.params.id;
+  const foundUser = users.find((e) => e.id == userId);
+  if (foundUser) {
+    next(); // next()--> allows the request to continue to the next middleware or function
+  } else {
+    res.send("tur yuqol");
+  }
 };
 
-app.get("/", middleware, (req, res) => {
-  res.send("salom");
+app.get("/users/:id", middleware, (req, res) => {
+  res.send(users);
+});
+
+app.post("/users/:id", middleware, (req, res) => {
+  res.send(users);
+});
+
+app.post("/users/:id", middleware, (req, res) => {
+  res.send(users);
 });
 app.listen(port, () => {
   console.log(`your 2nd lesson is running on https://localhost:${port}`);
