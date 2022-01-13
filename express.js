c339e061b90695b020253be6e76e15adbbab7f1f
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-13 15:46:43 +0500

    almost done

--- express.js
+++ express.js
@@ -1,45 +1,60 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const path = require("path");
 const port = 9999;
+app.use(express.json());
+//! app.use() to`liq application metodlari(Routelari) qabul qiluvchi middleware qabul qilish uchun ishlatiladi!
 
+const users = require("./modules/users.js");
 //! Middlewares
 /*
 "Middleware"lar routeimiz va controllerimiz"(req, res) => {})"orasida ishlab turuvchi funksiya 
---> authorisation va authentication uchun, errorlani ushlab olish uchun, keyinchalik "api" larimizni bloklab qo`yish uchun ishatiladi
+--> authorisation va authentication uchun, errorlani ushlab olish uchun, "api" larimizni bloklab qo`yish uchun ishatiladi va req, response abyectlariga ta`ssir ko`rsatish uchun ishlatiladi
 */
 
-const users = [
-  {
-    id: 1,
-    name: "Ali",
-  },
-  {
-    id: 2,
-    name: "Bekzod",
-  },
-];
+const userStatusCheck = (req, res, next) => {
+  const { name, token } = req.body;
 
-const middleware = (req, res, next) => {
-  const userId = req.params.id;
-  const foundUser = users.find((e) => e.id == userId);
+  const foundUser = users.find((e) => e.name == name && e.token == token);
   if (foundUser) {
+    req.body.role = foundUser.role;
     next(); // next()--> allows the request to continue to the next middleware or function
   } else {
-    res.send("tur yuqol");
+    res.status(401).send({
+      status: 401,
+      message: "You are not authorized",
+    });
   }
 };
 
-app.get("/users/:id", middleware, (req, res) => {
-  res.send(users);
+app.post("/login", userStatusCheck, (req, res) => {
+  const { role } = req.body;
+  if (role == "admin") {
+    res.redirect("/admin");
+  } else if (role == "subAdmin") {
+    res.redirect("/subAdmin");
+  } else if (role == "user") {
+    res.redirect("/user");
+  }
 });
 
-app.post("/users/:id", middleware, (req, res) => {
-  res.send(users);
+app.get("/admin", (req, res) => {
+  res.send(
+    "You are the main admin and you can create subAdmin😍 for doing this just in Routing part type 'subAdmin' with post metod and in its body input subAdmin`s name & id"
+  );
 });
 
-app.post("/users/:id", middleware, (req, res) => {
-  res.send(users);
+app.get("/user", (req, res) => {
+  res.send("user page");
 });
+
+app.get("/subAdmin", (req, res) => {
+  const { name, id } = req.body;
+
+  res.send("subAdmin page");
+});
+
 app.listen(port, () => {
   console.log(`your 2nd lesson is running on https://localhost:${port}`);
 });
